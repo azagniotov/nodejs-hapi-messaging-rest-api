4d7e8e9e8dbd72073a494b1c4685b24171b77b17
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-08 22:09:09 -0700

    Ignored index.js from Istanbul

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var hapiServer = require('./server/server');
-var server = hapiServer.listen(3000);
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
-});
+/* istanbul ignore next */
+(function () {
+    var hapiServer = require('./server/server');
+    var server = hapiServer.listen(3000);
+    server.start(function () {
+        console.log('Server running at:', server.info.uri);
+    });
+})();
