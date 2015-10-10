07ced5caa78f69308913ee97fa24a896d3f711bb
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-10 16:41:31 -0700

    Removed debug statements from index.js

--- index.js
+++ index.js
@@ -4,8 +4,5 @@
     var server = hapiServer.listen(3000);
     server.start(function () {
         console.log('Server running at:', server.info.uri);
-        //var table = server.table('localhost');
-        //var route = table[0].table[1];
-        //console.log(route.settings.id + ", " + route.method + ", " + route.path + ", " + route.settings.description);
     });
 })();
