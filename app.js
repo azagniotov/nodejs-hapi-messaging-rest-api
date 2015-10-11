12b42bdbde6f666fb660954afa1379a3ba787022
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-11 16:12:19 -0700

    Added SqlLite3

--- app.js
+++ app.js
@@ -8,4 +8,25 @@
         console.log('Server running at:', server.info.uri);
     });
     module.exports = {server: server};
+
+
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database(':memory:');
+
+    db.serialize(function() {
+
+        db.run("CREATE TABLE if not exists user_info (info TEXT)");
+        var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+        for (var i = 0; i < 10; i++) {
+            stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+
+        db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
+            console.log(row.id + ": " + row.info);
+        });
+    });
+
+    db.close();
+
 })();
