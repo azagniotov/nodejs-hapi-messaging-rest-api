c655bedc3a5ac12c22426d76607b40ba10d078d1
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-11 01:32:42 -0700

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-global.__project_root = __dirname + '/';
-global.__main_root = __project_root + 'main/';
-
 /* istanbul ignore next */
 (function () {
+    global.__project_root = __dirname + '/';
+    global.__main_root = __project_root + 'main/';
     var hapiServer = require(__main_root + 'server/Server');
     var server = hapiServer.listen();
     server.start(function () {
