c57148ec8c54ac6e3cb584dbed9afb16b34f7d48
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-18 22:51:26 -0400

    Code cleanup

--- main/db/DB.js
+++ main/db/DB.js
@@ -1,31 +1,21 @@
-"use strict";
+'use strict';
 
-var fs = require("fs");
+var fs = require('fs');
 var environment = process.env.NODE_ENV;
 
 var config = require(__main_root + 'config/database.js')[environment];
-var Sequelize = require("sequelize");
+var Sequelize = require('sequelize');
 var sequelizeInstance = new Sequelize(config.database, config.username, config.password, config);
-var db = {};
-db.models = {};
+var db = {models: {}};
 
 fs.readdirSync(__main_root + 'models/')
     .filter(function (file) {
-        return (file.indexOf(".") !== 0) && (file !== "index.js");
+        return (file.indexOf('.') !== 0);
     }).forEach(function (file) {
         var model = sequelizeInstance.import(__main_root + 'models/' + file);
         db.models[model.name] = model;
     });
 
-/*
- Object.keys(db).forEach(function (modelName) {
- if ("associate" in db[modelName]) {
- db[modelName].associate(db);
- }
- });
- */
-
 db.instance = sequelizeInstance;
-db.Sequelize = Sequelize;
 
 module.exports = db;
